eccc2584413d8719dc22ddcbc97ffb047879c951
rald <thecoconutnutisagiantnut@gmail.com>
2020-05-28 14:00:28 +0800

    update

--- PotionG-00/game.js
+++ PotionG-00/game.js
@@ -1,5 +1,28 @@
-var centerX=canvas.width/2;
-var centerY=canvas.height/2;
+var radius;
+var radiusIncr;
 
-setStrokeStyle("#FFFFFF");
-drawCircle(centerX,centerY,64);
+function draw() {
+	var centerX=canvas.width/2;
+	var centerY=canvas.height/2;
+
+	setFillStyle("#000000");
+	fillRect(0,0,canvas.width,canvas.height);
+
+	setFillStyle("#FFCBEA");
+	fillCircle(centerX,centerY,radius);
+
+	radius+=radiusIncr;
+
+	if(radius<0) { radius=0; radiusIncr=Math.abs(radiusIncr); }
+	if(radius>100) { radius=100; radiusIncr=-Math.abs(radiusIncr); }
+}
+
+function main() {
+
+	radius=0;
+	radiusIncr=4;
+
+	setInterval(draw,1000/60);
+}
+
+main();
